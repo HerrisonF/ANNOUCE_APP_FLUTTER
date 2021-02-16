8b70ce00d8a91f0201bbd296828316a1cf418224
Herrison Féres <herrison.feres@accurate.com.br>
2021-02-16 12:50:11 -0300

    app annouce

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get_it/get_it.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:xlo_mobx/screens/base/base_screen.dart';
+import 'package:xlo_mobx/stores/category_store.dart';
+import 'package:xlo_mobx/stores/connectivity_store.dart';
+import 'package:xlo_mobx/stores/favorite_store.dart';
+import 'package:xlo_mobx/stores/home_store.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
+import 'package:xlo_mobx/stores/user_manager_store.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeParse();
+  setupLocators();
+  runApp(MyApp());
+}
+
+void setupLocators() {
+  GetIt.I.registerSingleton(ConnectivityStore());
+  GetIt.I.registerSingleton(PageStore());
+  GetIt.I.registerSingleton(HomeStore());
+  GetIt.I.registerSingleton(UserManagerStore());
+  GetIt.I.registerSingleton(CategoryStore());
+  GetIt.I.registerSingleton(FavoriteStore());
+}
+
+Future<void> initializeParse() async {
+  await Parse().initialize(
+    'g7SpGW6DZVnjVtFnxGWRkFK9GzWPe5Nd3XpZWl6d',
+    'https://parseapi.back4app.com/',
+    clientKey: 'VfbVYwkN86nRoh5QdLoEvDRxv7tTujHsRuzwSgN4',
+    autoSendSessionId: true,
+    debug: true,
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'XLO',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.purple,
+        appBarTheme: AppBarTheme(elevation: 0),
+        cursorColor: Colors.orange,
+      ),
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+      ],
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      home: BaseScreen(),
+    );
+  }
+}
